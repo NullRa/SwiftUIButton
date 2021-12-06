2e75c8a68c7d868990aa5ea87a7ab8fb99f44161
jyang <jTang@walkgame.com>
2021-12-06 16:13:58 +0800

    Initial Commit

---
+++ SwiftUIButton/SwiftUIButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonApp.swift
+//  SwiftUIButton
+//
+//  Created by Apple on 2021/12/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
